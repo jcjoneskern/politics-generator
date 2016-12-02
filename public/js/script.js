97d428fc539fb78dfddb4b6a822acd7bee97edbb
J-C <jcjoneskern@gmail.com>
2016-12-02 10:45:58 -0500

    added Arthur

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@ $(function() {
     $.get('/api/quote', function(data){
       $("#quote").html(data);
     });
+    $(this).html() == "Be quiet!" ? $(this).html('I order you to be quiet!') : $(this).html('Be quiet!');
   });
 
   $.get('/api/quote', function(data){
